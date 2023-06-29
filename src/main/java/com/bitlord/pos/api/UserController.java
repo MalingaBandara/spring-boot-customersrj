724ecdb5e531db41e10245d9c19d8a3fdf500def
Malinga Bandara <malinga1021@gmail.com>
2023-06-29 14:58:59 +0530

    update api - User Controller

--- src/main/java/com/bitlord/pos/api/UserController.java
+++ src/main/java/com/bitlord/pos/api/UserController.java
@@ -1,7 +1,10 @@
 package com.bitlord.pos.api;
 
 import com.bitlord.pos.dto.request.RequestUserDto;
+import com.bitlord.pos.service.UserService;
 import com.bitlord.pos.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +13,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping( "/api/v1/user" )
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
 
     @PostMapping( params = "role" )
     public ResponseEntity<StandardResponse> createUser ( @RequestBody RequestUserDto dto, @RequestParam String role ) {
 
+        userService.createUser( dto, role ); // save user
 
+        return new ResponseEntity<>( new StandardResponse(201, "", null ), HttpStatus.CREATED );
 
     }
 
