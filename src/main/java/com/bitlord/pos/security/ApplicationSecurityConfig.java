e5f876bf8b0cac260e407061a72d0eada397942c
Malinga Bandara <malinga1021@gmail.com>
2023-06-29 20:31:37 +0530

    Add Http Security (Application Security Config)

--- src/main/java/com/bitlord/pos/security/ApplicationSecurityConfig.java
+++ src/main/java/com/bitlord/pos/security/ApplicationSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 
 @Configuration // Configuration class
@@ -14,6 +17,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         // Configuration
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("DELETE", "GET", "PUT", "POST", "PATCH", "OPTION"));
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+
+        //csrf().disable() => dont do that
+        http.csrf().disable()
+                .cors().configurationSource(request -> corsConfiguration)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/api/v1/user/register/**"
+                ).permitAll()
+                .anyRequest()
+                .permitAll();
 
     }
+
 }
